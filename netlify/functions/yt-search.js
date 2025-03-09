ff2e33325254764fbe6de99358a1b1498914807d
dogaodev <thhhmetods@gmail.com>
2025-03-09 15:31:59 -0300

    Forçando detecção de mudanças

--- netlify/functions/yt-search.js
+++ netlify/functions/yt-search.js
@@ -1,28 +1,30 @@
 const axios = require('axios');
 const fs = require('fs');
 
-// Carregar as chaves de API a partir do arquivo apis.json
-const apis = JSON.parse(fs.readFileSync('./apis.json'));
+// Carregar as chaves da API a partir do arquivo JSON
+const apis = JSON.parse(fs.readFileSync('./apis.json', 'utf8')).apis;
 
-// API do YouTube
-const YOUTUBE_API_KEY = apis.apis.find(api => api.name === "youtube").key;
+// Função para obter a chave da API pelo nome
+const getApiKey = (name) => {
+    const api = apis.find(api => api.name === name);
+    return api ? api.key : null;
+};
+
+const YOUTUBE_API_KEY = getApiKey("youtube");
 const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search";
 
-// Função que lida com a requisição
 exports.handler = async (event, context) => {
     const { search, api_key } = event.queryStringParameters;
 
     // Verifica se a chave da API foi fornecida e se é válida
-    const api = apis.apis.find(api => api.key === api_key);
-
-    if (!api) {
+    const validApiKey = getApiKey("visionario"); // Verificando se a chave é válida para o sistema
+    if (!api_key || api_key !== validApiKey) {
         return {
             statusCode: 403,
             body: JSON.stringify({ error: "Unauthorized. Invalid API Key." })
         };
     }
 
-    // Verifica se o parâmetro 'search' foi fornecido
     if (!search) {
         return {
             statusCode: 400,
