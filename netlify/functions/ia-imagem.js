62175ab1b0f49d1485989f2f322c2de01783a513
dogaodev <thhhmetods@gmail.com>
2025-03-09 14:23:30 -0300

    Forçando detecção de mudanças

--- netlify/functions/ia-imagem.js
+++ netlify/functions/ia-imagem.js
@@ -35,7 +35,7 @@ exports.handler = async (event, context) => {
             headers: {
                 "User-Agent": "Mozilla/5.0" // Adiciona um User-Agent para evitar bloqueios
             },
-            timeout: 30000 // Timeout de 30 segundos para garantir que aguarde a resposta
+             // Timeout de 30 segundos para garantir que aguarde a resposta
         });
 
         // Verifica se a resposta contém a URL da imagem gerada
